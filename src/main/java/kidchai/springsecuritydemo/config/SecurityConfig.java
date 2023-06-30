5f9760837d3d17f5d7a9e5534aace5a640e4a23c
Kidchai <o.naibova@gmail.com>
2023-06-30 13:06:48 -0500

    Replace deprecated .authorizeRequests with .authorizeHttpRequests

--- src/main/java/kidchai/springsecuritydemo/config/SecurityConfig.java
+++ src/main/java/kidchai/springsecuritydemo/config/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
-                .authorizeRequests(authorize -> authorize
+                .authorizeHttpRequests(requests -> requests
                         .requestMatchers("/auth/login", "/auth/register", "/error").permitAll()
                         .anyRequest().authenticated())
                 .formLogin(form -> form
